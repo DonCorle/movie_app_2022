bb6840889812feecaae03c08cc181b15372c8e10
stpark <dmc0110@naver.com>
2022-06-10 15:43:45 +0900

    업로드

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-function Food(props){
-  console.log(props);
-  return <h3>I like Potato</h3>;
+function Food({fav}){
+  return <h3>I like {fav}</h3>;
 }
 function App() {
   return(
     <div>
       <h1>Hello</h1>
-      <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
+      <Food fav="kimchi"/>
+      <Food fav="kimbap"/>
+      <Food fav="ramen"/>
+      <Food fav="udon"/>
     </div>
   );
 }

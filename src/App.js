019b814832cdb318371a387fe47dd16a2efa626d
stpark <dmc0110@naver.com>
2022-06-10 15:29:20 +0900

    업로드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+function Food(props){
+  console.log(props);
+  return <h3>I like Potato</h3>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
     </div>
   );
 }
